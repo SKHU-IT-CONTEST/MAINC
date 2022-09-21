eaf7c7617aae3e5e3ecf5d6643e30db68f949607
nmmn1004 <51752838+nmmn1004@users.noreply.github.com>
2022-09-21 14:00:08 +0900

    .

--- js/button.js
+++ js/button.js
@@ -3,13 +3,13 @@ var data = arr;
 
 
 function CreateButton() {
-    document.getElementById("plus-button").style.display ='none';
+	document.getElementById("plus-button").style.visibility = "hidden";
 
     var InputArea = document.getElementById('input-area');
     var Input = document.createElement('div');
     InputArea.appendChild(Input);
 
-    Input.innerHTML = "<input type=\"text\" id=\"answer\" onkeypress=\"if(event.keyCode==13){TextBox();}\">";
+    Input.innerHTML = "<input type=\"text\" id=\"answer\" class=\"button-style\" onkeypress=\"if(event.keyCode==13){TextBox();}\">";
 }
 
 function TextBox() {
@@ -20,14 +20,24 @@ function TextBox() {
 	}
 	
 	else{
-		var cont = document.getElementById('container');// html에 있는 container id를 cont에 부여
-		var Btn = document.createElement('button');
-		cont.appendChild(Btn)
-		Btn.className = 'btn';
+		var container = document.getElementById('container');// html에 있는 container id를 cont에 부여
 
-		var BtnText = document.createTextNode(answer);
-		Btn.appendChild(BtnText);
+		
+		container.innerHTML = "<button id=\"answer-button\" class=\"button-style\"></button>";
+		var AnswerButton = document.getElementById('answer-button');
+		container.appendChild(AnswerButton);
+
+		var AnswerText = document.createTextNode(answer);
+		AnswerButton.appendChild(AnswerText);
+
+		var HeartText = document.createElement('heart');
+		HeartText.className = 'heart-style';
+		HeartText.innerHTML = "♥";
+		AnswerButton.appendChild(HeartText);
 
 		document.getElementById('answer').value = ""; //입력 칸을 빈칸으로
+		document.getElementById("plus-button").style.visibility ='visible';
+		document.getElementById("answer").style.visibility = "hidden";
+
 	}
 }
