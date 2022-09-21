3d5e1c3dc1c665663232be306509a9e692644fd6
seulseul_0123 <104067367+yeaseul7@users.noreply.github.com>
2022-09-21 13:58:36 +0900

    js_part1_merge
    part1 부분 날씨 가져오는 js merge하겠습니다.

--- fe.js
+++
@@ -1,8 +0,0 @@
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-fetch('http://152.67.207.160:32023/api/schoolNotice')
-    .then(response => response.json())
-    .then(json => { 
-        console.log(json[0].num) 
-        const el = document.querySelector('#temperature')
-        el.innerHTML=json[0].num
-    })
--- js/button.js
+++ js/button.js
@@ -31,3 +31,11 @@ function TextBox() {
 		document.getElementById('answer').value = ""; //입력 칸을 빈칸으로
 	}
 }
+//온도 fetch 이용해서 임의로 가져온것
+fetch('http://152.67.207.160:32023/api/schoolNotice')
+    .then(response => response.json())
+    .then(json => { 
+        console.log(json[0].num) 
+        const el = document.querySelector('#temperature')
+        el.innerHTML=json[0].num
+    })
