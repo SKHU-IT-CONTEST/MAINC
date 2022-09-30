2a1a52f78c27cfb55bf4dff23519e48a9ad29042
nmmn1004 <51752838+nmmn1004@users.noreply.github.com>
2022-09-30 11:16:46 +0900

    add: 금칙어 기능 추가

--- js/button.js
+++ js/button.js
@@ -1,29 +1,18 @@
-arr = [];
-var data = arr;
 
-// // +버튼을 눌러 input박스를 생성
-// function CreateButton() {
-// 	document.getElementById("plus-button").style.visibility = "hidden";
-
-//     var InputArea = document.getElementById('input-area');
-//     var Input = document.createElement('div');
-//     InputArea.appendChild(Input);
-
-//     Input.innerHTML = "<input type=\"text\" id=\"answer\" class=\"button-style\" onkeypress=\"if(event.keyCode==13){TextBox();}\">";
-// }
+checkStr = new Array("금지어1","금지어2","금지어3","금지어4");
+limit = checkStr.length; // 금지어 배열의 갯수
 
 function TextBox() {
-
 	let answertemp = document.getElementById('answer').value;
 	if(answertemp == ""){
 		alert('응답을 적어주세요!');
 	}
-	
+	else if(checkIt(answertemp, limit)) {
+		alert('금지어입니다.');
+	}
 	else{
 		var container = document.getElementById('container');// html에 있는 container id를 cont에 부여
 		
-		// container.innerHTML = "<button id=\"answer-button\" class=\"button-style\"></button>";
-		// var AnswerButton = document.getElementById('answer-button');
 		let AnswerButton = document.createElement('button');
 		AnswerButton.className = 'buttonStyle';
 		AnswerButton.id = 'answerButton';
@@ -39,10 +28,26 @@ function TextBox() {
 		AnswerButton.appendChild(HeartText);
 
 		document.getElementById('answer').value = ""; //입력 칸을 빈칸으로
-		
-		// document.getElementById("plus-button").style.visibility ='visible';
-		// document.getElementById("answer").remove();
-
-		// 두 번째 버튼을 생성시 첫 번째 버튼이 사라지는 현상 해결해야함
+	}
+}
+// 비속어 찾는 for문
+function checkIt(v, l) {
+ for(i=0;i<l;i++)
+ {
+  if(v.indexOf(checkStr[i]) != -1)
+  {
+   return 1;
+  }
+ }
+ return 0;
+}
+// 특수문자 입력 방지
+function characterCheck(obj){
+	var regExp = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi; 
+	// 허용할 특수문자는 여기서 삭제하면 됨
+	// 지금은 띄어쓰기도 특수문자 처리됨 참고하셈
+	if( regExp.test(obj.value) ){
+		alert("특수문자는 입력하실수 없습니다.");
+		obj.value = obj.value.substring( 0 , obj.value.length - 1 ); // 입력한 특수문자 한자리 지움
 	}
 }
