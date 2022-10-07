6b858c7cdda75d88a15480c2bff8e09a1120c162
seulseul_0123 <104067367+yeaseul7@users.noreply.github.com>
2022-10-08 01:57:11 +0900

    모의 수정

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,29 @@
 //온도 fetch 이용해서 임의로 가져온것
+//온도
 fetch('https://api3.skhuweather.kro.kr/getWeather')
     .then(response => response.json())
     .then(json => { 
-        console.log(json[20221007.1800].PTY) 
+        console.log(json[20221007.1600].T1H) 
         const el = document.querySelector('#temperature')
-        el.innerHTML=json[20221007.1800].PTY
+        el.innerHTML=json[20221007.1600].T1H
     })
-//
+//강수량
+fetch('https://api3.skhuweather.kro.kr/getWeather')
+    .then(response => response.json())
+    .then(json => { 
+        console.log(json[20221007.1600].RN1) 
+        const el = document.querySelector('#cloudyData')
+        el.innerHTML=json[20221007.1600].RN1
+    })
+//습도
+fetch('https://api3.skhuweather.kro.kr/getWeather')
+    .then(response => response.json())
+    .then(json => { 
+        console.log(json[20221007.1600].REH) 
+        const el = document.querySelector('#waterData')
+        el.innerHTML=json[20221007.1600].REH
+    })
+//모바일 확인 js 코드
     function detectMobileDevice(agent) {
         const mobileRegex = [
           /Android/i,
