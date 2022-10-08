77fb169955705d6a722225017755c95b84176e66
seulseul_0123 <104067367+yeaseul7@users.noreply.github.com>
2022-10-08 14:55:17 +0900

    js이용해서 데이터 가져오기

--- js/weather.js
+++ js/weather.js
@@ -1,27 +1,37 @@
 //온도 fetch 이용해서 임의로 가져온것
+
+//json코드가 배열이 아니기 때문에 index로 못 가져온다.
+
+const date = new Date();//Date 객체 생성
+const year = date.getFullYear(); //년도 가져오기
+const month =String(date.getMonth()+1).padStart(2, "0");  //2자릿수 만들고 왼쪽부터 0넣기(데이터가 2자릿수이면 2자릿수 그대로 출력)
+const day = String(date.getDate()).padStart(2, "0");  //month와 동일한 방식
+const hours = String(date.getHours()).padStart(2, "0"); //month와 동일한 방식
+const reIndex = `${year}${month}${day}.${hours}00`; //임시 index라는 의미, 백틱 사용해서 json 형태와 똑같이 뽑아내기, 분은 안뽑아냄.
+
 //온도
 fetch('https://api3.skhuweather.kro.kr/getWeather')
     .then(response => response.json())
     .then(json => { 
-        console.log(json[0].T1H) 
+        console.log(json[reIndex].T1H) //원래 '20221004.1400' 넣어야 할 부분에 위에서 가져온 데이터와 비교하여 일치하면 넣기 
         const el = document.querySelector('#temperature')
-        el.innerHTML=json[0].T1H
+        el.innerHTML=json[reIndex].T1H
     })
 //강수량
 fetch('https://api3.skhuweather.kro.kr/getWeather')
     .then(response => response.json())
     .then(json => { 
-        console.log(json['20221008.1200'].RN1) 
+        console.log(json[reIndex].RN1) 
         const el = document.querySelector('#cloudyData')
-        el.innerHTML=json['20221008.1200'].RN1
+        el.innerHTML=json[reIndex].RN1
     })
 //습도
 fetch('https://api3.skhuweather.kro.kr/getWeather')
     .then(response => response.json())
     .then(json => { 
-        console.log(json['20221008.1200'].REH) 
+        console.log(json[reIndex].REH) 
         const el = document.querySelector('#waterData')
-        el.innerHTML=json['20221008.1200'].REH
+        el.innerHTML=json[reIndex].REH
     })
 //모바일 확인 js 코드
     function detectMobileDevice(agent) {
