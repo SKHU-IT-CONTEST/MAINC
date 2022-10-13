a21bb92531c81fe06e014766adb43962f7e16c09
Hwang JaeHyeon <72461790+ten0213@users.noreply.github.com>
2022-10-13 23:55:01 +0900

    날씨제보 기능의 backend 부분 수정(황재현)

--- js/count.js
+++ js/count.js
@@ -16,7 +16,10 @@ function doubleSubmitCheck() {
     }
 
 }
+
+
 function ready() {
+    resetData();
     var textHolder1 = document.getElementById("count1");
     var textHolder2 = document.getElementById("count2");
     var textHolder3 = document.getElementById("count3");
@@ -29,7 +32,44 @@ function ready() {
     textHolder4.innerHTML = localStorage.getItem("data4");
     textHolder5.innerHTML = localStorage.getItem("data5");
     textHolder6.innerHTML = localStorage.getItem("data6");
+
+    function resetData() {
+        let nowDate = new Date();
+
+        if (nowDate.getHours() === 0o3 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00 ||
+            nowDate.getHours() === 0o6 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00 ||
+            nowDate.getHours() === 9 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00 ||
+            nowDate.getHours() === 12 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00 ||
+            nowDate.getHours() === 15 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00 ||
+            nowDate.getHours() === 18 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00 ||
+            nowDate.getHours() === 21 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00 ||
+            nowDate.getHours() === 0o0 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00) {
+            for (let i = 0; i < 6; i++) {
+                var countTmp_1 = localStorage.getItem("data1");
+                countTmp_1 = 0;
+                var countTmp_2 = localStorage.getItem("data2");
+                countTmp_2 = 0;
+                var countTmp_3 = localStorage.getItem("data3");
+                countTmp_3 = 0;
+                var countTmp_4 = localStorage.getItem("data4");
+                countTmp_4 = 0;
+                var countTmp_5 = localStorage.getItem("data5");
+                countTmp_5 = 0;
+                var countTmp_6 = localStorage.getItem("data6");
+                countTmp_6 = 0;
+
+                localStorage.setItem("data1", countTmp_1.toString());
+                localStorage.setItem("data2", countTmp_2.toString());
+                localStorage.setItem("data3", countTmp_3.toString());
+                localStorage.setItem("data4", countTmp_4.toString());
+                localStorage.setItem("data5", countTmp_5.toString());
+                localStorage.setItem("data6", countTmp_6.toString());
+            }
+        }
+    }
+
 }
+
 function insert() {
     let count1 = 0;
     let count2 = 0;
@@ -37,7 +77,7 @@ function insert() {
     let count4 = 0;
     let count5 = 0;
     let count6 = 0;
-    var count_arr = [0, 0, 0, 0, 0, 0]
+
 
     const button1 = document.getElementById("button1");
     const button2 = document.getElementById("button2");
@@ -51,8 +91,6 @@ function insert() {
     var textHolder4 = document.getElementById("count4");
     var textHolder5 = document.getElementById("count5");
     var textHolder6 = document.getElementById("count6");
-
-
 
 
     button1.addEventListener("click", function () {
@@ -106,99 +144,59 @@ function insert() {
 
     });
 
-    textHolder1 = document.getElementById("count1");
-    textHolder2 = document.getElementById("count2");
-    textHolder3 = document.getElementById("count3");
-    textHolder4 = document.getElementById("count4");
-    textHolder5 = document.getElementById("count5");
-    textHolder6 = document.getElementById("count6");
-    var textHolder_arr = [textHolder1, textHolder2, textHolder3, textHolder4, textHolder5, textHolder6];
-
-
-    var arr = [count1, count2, count3, count4, count5, count6];
-    resetData();
-    let tmp = 0;
-
 
     // 입력값의 최대값 탐색
-    for (let j = 0; j < arr.length; j++) {
-        let tmp = 0;
-        if (tmp < arr[j]) {
-            tmp = arr[j];
-        }
 
-    }
 
-    for (let k = 0; k < textHolder_arr.length; k++) {
-        if (textHolder_arr[k] === arr[k]) {
-            const initText = document.getElementById('skhu-turtle');
-            initText.appendChild(tmp);
-        }
 
-    }
 
-    function resetData() {
-        let nowDate = new Date();
 
-        if (nowDate.getHours() === 0o3 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00 ||
-            nowDate.getHours() === 0o6&& nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00 ||
-            nowDate.getHours() === 9 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00 ||
-            nowDate.getHours() === 12 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00 ||
-            nowDate.getHours() === 15 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00||
-            nowDate.getHours() === 18 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00 ||
-            nowDate.getHours() === 21 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00 ||
-            nowDate.getHours() === 0o0 && nowDate.getMinutes() === 0o0 && nowDate.getSeconds() === 0o0 && nowDate.getMilliseconds() === 0o00) {
-            for (let i = 0; i < count_arr.length; i++) {
-               var countTmp_1 = localStorage.getItem("data1"); countTmp_1 = 0;
-               var countTmp_2 = localStorage.getItem("data2"); countTmp_2 = 0;
-               var countTmp_3 = localStorage.getItem("data3"); countTmp_3 = 0;
-               var countTmp_4 = localStorage.getItem("data4"); countTmp_4 = 0;
-               var countTmp_5 = localStorage.getItem("data5"); countTmp_5 = 0;
-               var countTmp_6 = localStorage.getItem("data6"); countTmp_6 = 0;
 
-               localStorage.setItem("data1", countTmp_1.toString());
-               localStorage.setItem("data2", countTmp_2.toString());
-               localStorage.setItem("data3", countTmp_3.toString());
-               localStorage.setItem("data4", countTmp_4.toString());
-               localStorage.setItem("data5", countTmp_5.toString());
-               localStorage.setItem("data6", countTmp_6.toString());
-            }
-        }
-    }
 
     function countData1() {
-        let countOneOnclick_1 = localStorage.getItem("data1"); countOneOnclick_1++;
+        let countOneOnclick_1 = localStorage.getItem("data1");
+        countOneOnclick_1++;
         localStorage.setItem("data1", countOneOnclick_1.toString());
         return countOneOnclick_1;
 
     }
+
     function countData2() {
-        let countOneOnclick_2 = localStorage.getItem("data2"); countOneOnclick_2++;
+        let countOneOnclick_2 = localStorage.getItem("data2");
+        countOneOnclick_2++;
         localStorage.setItem("data2", countOneOnclick_2.toString());
         return countOneOnclick_2;
     }
 
 
     function countData3() {
-        let countOneOnclick_3 = localStorage.getItem("data3"); countOneOnclick_3++;
+        let countOneOnclick_3 = localStorage.getItem("data3");
+        countOneOnclick_3++;
         localStorage.setItem("data3", countOneOnclick_3.toString());
         return countOneOnclick_3;
     }
+
     function countData4() {
-        let countOneOnclick_4 = localStorage.getItem("data4"); countOneOnclick_4++;
+        let countOneOnclick_4 = localStorage.getItem("data4");
+        countOneOnclick_4++;
         localStorage.setItem("data4", countOneOnclick_4.toString());
         return countOneOnclick_4;
 
     }
+
     function countData5() {
-        let countOneOnclick_5 = localStorage.getItem("data5"); countOneOnclick_5++;
+        let countOneOnclick_5 = localStorage.getItem("data5");
+        countOneOnclick_5++;
         localStorage.setItem("data5", countOneOnclick_5.toString());
         return countOneOnclick_5;
     }
+
     function countData6() {
-        let countOneOnclick_6 = localStorage.getItem("data6"); countOneOnclick_6++;
+        let countOneOnclick_6 = localStorage.getItem("data6");
+        countOneOnclick_6++;
         localStorage.setItem("data6", countOneOnclick_6.toString());
         return countOneOnclick_6;
 
     }
+
 }
--- js/view.js
+++ js/view.js
@@ -1,1 +1,52 @@
-const button = document.querySelector("button");
+function viewMaxCount() {
+    var tmp = 0;
+
+    let front_1 = localStorage.getItem("data1");
+    let front_2 = localStorage.getItem("data2");
+    let front_3 = localStorage.getItem("data3");
+    let front_4 = localStorage.getItem("data4");
+    let front_5 = localStorage.getItem("data5");
+    let front_6 = localStorage.getItem("data6");
+    let front_arr = [front_1, front_2, front_3, front_4, front_5, front_6];
+    for (let i = 0; i < 6; i++) {
+        if (tmp < front_arr[i]) {
+            tmp = front_arr[i]
+        }
+    }
+
+    for (let j = 0; j < 6; j++) {
+        const element1 = document.querySelector('#turtle-icon');
+        if (front_arr[j] === tmp) {
+            var img_tmp = new Image();
+            if (j === 0) {
+                img_tmp.src = `./img/report/report_rainy.png`;
+                element1.innerHTML = (img_tmp);
+            }
+            else if(j === 1) {
+                img_tmp.src=`./img/report/report_cloudy.png`;
+                element1.innerHTML = (img_tmp);
+
+            }
+            else if(j===2) {
+                img_tmp.src = `./img/report/report_sunny.png`;
+                element1.innerHTML = (img_tmp);
+            }
+            else if(j === 3) {
+                img_tmp.src= `img/report/report_dust.png`;
+                element1.innerHTML = (img_tmp);
+            }
+            else if(j === 4) {
+                img_tmp.src = `img/report/report_windy.png`;
+                element1.innerHTML = (img_tmp);
+
+            }
+            else if(j === 5) {
+                img_tmp = `img/report/report_snow.png`;
+                element1.innerHTML =(img_tmp);
+            }
+
+        }
+    }
+
+
+}
