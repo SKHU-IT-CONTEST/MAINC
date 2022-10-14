54bb60bd0f08a151168f5f4e340644fe4da4df6f
seulseul_0123 <104067367+yeaseul7@users.noreply.github.com>
2022-10-14 11:51:53 +0900

    cloudy_40
    40은 ksloudy가 아니고 cloudy2입니당

--- js/weather.js
+++ js/weather.js
@@ -148,7 +148,7 @@ const reIndex3 =`${year}${month}${day}.${hours3}00`;
          console.log(SKY, PTY);
          const code = `${SKY}${PTY}`;
          const weatherIconMap = {
-             10: 'ksun',11: 'ksun',41: 'cloudyRain2',42: 'cloudyRain2',45: 'cloudyRain2',31: 'krain',32: 'krain',35: 'krain',30: 'kcloudy',40: 'kcloudy',33: 'ksnow',36: 'ksnow',37: 'ksnow',43: 'cloudySnow2',46: 'cloudySnow2',47: 'cloudySnow2',
+             10: 'ksun',11: 'ksun',41: 'cloudyRain2',42: 'cloudyRain2',45: 'cloudyRain2',31: 'krain',32: 'krain',35: 'krain',30: 'kcloudy',40: 'cloudy2',33: 'ksnow',36: 'ksnow',37: 'ksnow',43: 'cloudySnow2',46: 'cloudySnow2',47: 'cloudySnow2',
          };
          console.log(weatherIconMap);
          element33.innerHTML=`<img class="weather-kicon" src="img/icon/${weatherIconMap[code]}.png"/>`;
